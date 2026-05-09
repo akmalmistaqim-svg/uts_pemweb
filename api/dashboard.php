0f3edbce260704ac30514240627a5400b9b8a739
Akmal Mustaqim <akmalmistaqim@gmail.com>
2026-05-09 10:34:51 +0700

    up

--- api/dashboard.php
+++ api/dashboard.php
@@ -177,7 +177,7 @@ if (!isset($_SESSION['id_pengguna'])) {
 
   </section>
 
-  <?php include 'api/grafik.php'; ?>
+  <?php include 'grafik.php'; ?>
 
   <!-- section tentang -->
   <section id="tentang" class="section-tentang py-16 px-6">
